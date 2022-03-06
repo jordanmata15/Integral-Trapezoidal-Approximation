75fb2440f6a8d17d12884b2532328fbbd02d374a
Jordan Mata <jmata9370@sdsu.edu>
2022-03-06 13:11:14 -0800

    Updated function to have more precision.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char** argv){
   ArgParser ap = ArgParser();
   Arguments* args = ap.parseArgs(argc, argv);
   DataManager manager = DataManager();
-  std::function<double(double)> myFunction = [](double x){ return 4/(1+(x*x)); }; // function to approximate pi
+  std::function<long double(long double)> myFunction = [](long double x){ return 4/(1+(x*x)); }; // function to approximate pi
   IntegralApproximation ia = IntegralApproximation(args->numThreads, args->numTrapezoids, &manager, &myFunction);
   
   std::cout << std::fixed << std::setprecision(20) << ia.runApproximation(0.0, 1.0) << "," << manager.average() << std::endl;
