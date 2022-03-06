4c0229ab012ac3073185280efab9a103d3938fc5
Jordan Mata <jmata9370@sdsu.edu>
2022-03-06 12:16:46 -0800

    Added header guards.

--- src/main.hpp
+++ src/main.hpp
@@ -1,5 +1,10 @@
+#ifndef __MAIN_HPP__
+#define __MAIN_HPP__
+
 #include <iomanip>
 #include <functional>
 #include "ArgParser.hpp"
 #include "DataManager.hpp"
 #include "IntegralApproximation.hpp"
+
+#endif // __MAIN_HPP__
